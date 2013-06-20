93bfe6727c01239520f92c7b81fa9b4e3679ac65
Alexander Rodin <rodin.alexander@gmail.com>
2013-06-20 23:10:37 +0400

    change omnauth-google-apps to omniauth-google-oauth2

--- lib/sinatra/google-auth.rb
+++ lib/sinatra/google-auth.rb
@@ -1,4 +1,4 @@
-require 'omniauth-google-apps'
+require 'omniauth-google-oauth2'
 require 'openid/store/filesystem'
 
 module Sinatra
@@ -10,11 +10,11 @@ module Sinatra
       end
 
       def call(env)
-        if env['rack.session']["user"] || env['REQUEST_PATH'] =~ /\/auth\/google_apps/
+        if env['rack.session']["user"] || env['REQUEST_PATH'] =~ /\/auth\/google_oauth2/
           @app.call(env)
         else
           env['rack.session']['google-auth-redirect'] = env['REQUEST_PATH']
-          location = File.join(env['REQUEST_PATH'], '/auth/google_apps')
+          location = File.join(env['REQUEST_PATH'], '/auth/google_oauth2')
           return [301, {'Content-Type' => 'text/html', 'Location' => location}, []]
         end
       end
@@ -25,48 +25,49 @@ module Sinatra
         unless session["user"]
           session['google-auth-redirect'] = request.path
           if settings.absolute_redirect?
-            redirect "/auth/google_apps"
+            redirect "/auth/google_oauth2"
           else
-            redirect to "/auth/google_apps"
+            redirect to "/auth/google_oauth2"
           end
         end
       end
-
+      
       def handle_authentication_callback
         unless session["user"]
           user_info = request.env["omniauth.auth"].info
           on_user(user_info) if respond_to? :on_user
           session["user"] = Array(user_info.email).first.downcase
         end
-
+        
         url = session['google-auth-redirect'] || to("/")
         redirect url
       end
     end
-
+    
     def self.secret
       ENV['SESSION_SECRET'] || ENV['SECURE_KEY'] || 'please change me'
     end
-
+    
     def self.registered(app)
-      raise "Must supply ENV var GOOGLE_AUTH_DOMAIN" unless ENV['GOOGLE_AUTH_DOMAIN']
+      raise "Must supply ENV var GOOGLE_CLIENT_ID" unless ENV['GOOGLE_CLIENT_ID']
+      raise "Must supply ENV var GOOGLE_CLIENT_SECRET" unless ENV['GOOGLE_CLIENT_SECRET']
       app.helpers GoogleAuth::Helpers
       app.use ::Rack::Session::Cookie, :secret => secret
       app.use ::OmniAuth::Builder do
-        provider :google_apps, :domain => ENV['GOOGLE_AUTH_DOMAIN']
+        provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
       end
-
+      
       app.set :absolute_redirect, false
-
+      
       app.get "/auth/:provider/callback" do
         handle_authentication_callback
       end
-
+      
       app.post "/auth/:provider/callback" do
         handle_authentication_callback
       end
     end
   end
-
+  
   register GoogleAuth
 end
